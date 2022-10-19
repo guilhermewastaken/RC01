ed2ca45fa87eb9f236d0021110e5fbd820db65f0
guilhermewastaken <75986527+guilhermewastaken@users.noreply.github.com>
2022-10-19 10:08:19 +0100

    Added receivePacket

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+// Link layer protocol implementation
+
+#include "link_layer.h"
 
 enum MACHINE {TRANSMITTER = 0, RECEIVER = 1};                                           //Machine constants
 enum HEADER_TYPE {INVALID = -1, INFO, SET, DISC, UA, RR, REJ};
 enum BYTE {FLAG = 0x7e, ESCAPE = 0x7d, ESC_FLAG = 0x5e, ESC_ESCAPE = 0x5d,              //Flag and byte stuffing
-        A_TRANSMITTER_COMMAND = 0x03, A_RECEIVER_COMMAND = 0x01,                        //Address
-        CNTRL_INFO_0 = 0x00, CNTRL_INFO_1 = 0x40, CNTRL_SET = 0x03, CNTRL_DISC = 0x0b,  //Control Commands
-        CNTRL_UA = 0x07, CNTRL_RR_0 = 0x05, CNTRL_RR_1 = 0x85,
-        CNTRL_REJ_0 = 0x01, CNTRL_REJ_1 = 0x81};                                        //Control responses
+    A_TRANSMITTER_COMMAND = 0x03, A_RECEIVER_COMMAND = 0x01,                        //Address
+    CNTRL_INFO_0 = 0x00, CNTRL_INFO_1 = 0x40, CNTRL_SET = 0x03, CNTRL_DISC = 0x0b,  //Control Commands
+    CNTRL_UA = 0x07, CNTRL_RR_0 = 0x05, CNTRL_RR_1 = 0x85,
+    CNTRL_REJ_0 = 0x01, CNTRL_REJ_1 = 0x81};                                        //Control responses
 
 int machine;    //0 if transmitter or 1 if receiver
 int messageParity = 0;  //0 or 1, switches
@@ -88,7 +90,7 @@ int createHeader(unsigned char *header, int type) { //Tested, header must have 4
     return 0;
 }
 
-int getHeaderType(unsigned char *header) {
+int getHeaderType(unsigned char *header, int machine) {
     //1- Check BCC
     if (header[3] != getBCC(header, 3)) {
         return INVALID;
@@ -189,169 +191,41 @@ int removeStuffing(unsigned char *content, int size) {
         }
         t++;
     }
-
-
     return newSize;
 }
 
-void alarmHandler(int signal)
+// MISC
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+int llopen(LinkLayer connectionParameters)
 {
-    alarmEnabled = 0;
-    alarmCount++;
+    // TODO
 
+    return 1;
 }
-int receivePacket(unsigned char *data, int *size) {
-    //Internal State Machine
-    enum STATE_MACHINE {WAIT_FOR_FLAG = 0, BUILDING_HEADER, WAIT_FOR_LAST_FLAG, FILLING_INFO, OVER};
-    unsigned char byteReceived;
-    int bytes;
-    int counter = 0;
-    int state = WAIT_FOR_FLAG;
-    int header = INVALID;
 
-    while(alarmEnabled == 1 && state != OVER){
-        switch (state){
-            case WAIT_FOR_FLAG:
-                bytes = read(fd,byteReceived,1);
-                if (bytes != 0 && byteReceived == FLAG) {
-                    data[0] = FLAG;
-                    state = BUILDING_HEADER;
-                }
-                break;
-            case BUILDING_HEADER:
-                bytes = read(fd,byteReceived,1);
-                if (bytes != 0) {
-                    if (byteReceived == FLAG) {
-                        counter = 0;
-                    }
-                    else {
-                        counter++;
-                        data[counter] = byteReceived;
-                    }
-                }
-                if (counter == 3) {
-                    header = getHeaderType(data);
-                    if (header == INVALID) {
-                        state = WAIT_FOR_FLAG;
-                        counter = 0;
-                    }
-                    else if (header == INFO) {
-                        counter = 0;
-                        state = FILLING_INFO;
-                    }
-                    else {
-                        state = WAIT_FOR_LAST_FLAG;
-                    }
-                }
-            case WAIT_FOR_LAST_FLAG:
-                bytes = read(fd,byteReceived,1);
-                if (bytes != 0 && byteReceived == FLAG) {
-                    state = OVER;
-                }
-                else if (bytes != 0) {
-                    state = WAIT_FOR_FLAG;
-                    counter = 0;
-                }
-            case FILLING_INFO:
-                bytes = read(fd,byteReceived,1);
-                if (bytes != 0 && byteReceived == FLAG) {
-                    *size = counter;
-                    state = OVER;
-                }
-                else if (bytes != 0) {
-                    data[counter] = byteReceived;
-                    counter++;
-                }
-        }
-    }
-    //Check BCC and send REJ if wrong
-    return header;
-}
-int sendPacket(int type, unsigned char * data, int dataSize) {
-    unsigned char msg[20];
-    int acknowledge = 0;
-    int alarmCount = 0;
-    int alarmEnabled = 1;
+int llwrite(const unsigned char *buf, int bufSize)
+{
+    // TODO
 
-    (void)signal(SIGALRM, alarmHandler);
-
-    while (!acknowledge) {
-        if (createHeader(msg, INFO) != 0) {
-            return -1;
-        }
-
-        if (write(fd, msg, 4) != 4) {
-            return -1;
-        }
-
-        if (type == INFO) {
-            for (int i = 0; i < dataSize; i++) {
-                msg[i] = data[i];
-            }
-            msg[dataSize] = getBCC(data, dataSize);
-            msg[dataSize + 1] = FLAG;
-            newSize = addStuffing(msg, dataSize);
-            if (write(fd, msg, newSize) != newSize) {
-                return -1;
-            }
-        }
-
-        else { // if it doesn't have data
-            msg[0] = FLAG;
-            if (write(fd, msg, 1) != 1) {
-                return -1;
-            }
-        }
-        alarm(10);
-        int typeResponse;
-        typeResponse = receivePacket();
-
-        if (type == SET) {
-            
-        }
-
-        if (type == DISC) {
-            if (machine == TRANSMITTER) {
-                if (typeResponse == DISC) {
-                    acknowledge = 1;
-                    alarm(0);
-                }
-            }
-            else if (machine == RECEIVER) {
-                if (typeResponse = UA) {
-                    acknowledge = 1;
-                    alarm(0);
-                }
-            }
-        }
-
-        if (type == info) {
-            if (messageParity == 0) {
-                // TO DO
-            }
-        }
-
-        if (type == UA || type == RR || type == REJ) {
-            acknowledge = 1;
-            alarm(0);
-        }
-
-        alarmEnabled = 1;    
-    }
-
-    if (messageParity == 0) {
-        messageParity = 1;
-    }
-    else if (messageParity == 1) {
-        messageParity = 0;
-    }
     return 0;
 }
 
-int main() {
-    unsigned char array1[10] = {0x00, ESCAPE,0x0F, FLAG, 0xFF, FLAG, 0x00, 0x00, 0x00, 0x00};
-    printf("%i", addStuffing(array1, 6));
+int llread(unsigned char *packet)
+{
+    // TODO
+
     return 0;
 }
 
+int llclose(int showStatistics)
+{
 
+    return 1;
+}
+
+unsigned char receivePacket(unsigned char *info) {
+    //Receive header
+
+    //Receive information
+}
