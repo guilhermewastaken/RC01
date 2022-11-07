a2229450cdb645a5c7f87cb990f58616b2a3d783
guilhermewastaken <75986527+guilhermewastaken@users.noreply.github.com>
2022-11-07 15:29:43 +0000

    Revert "Debug"
    This reverts commit 42a004053b9bc7c13dafcb59381cfd80c5534681.

--- code_final/code/src/application_layer.c
+++ code_final/code/src/application_layer.c
@@ -133,13 +133,8 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
         int counter = 0;
         while (!feof(filePtr)) {
             printf("Sent a frame\n");
-            int currentI = 0;
-            while (!feof(filePtr) && currentI < INPUT_SIZE) {
-                fread(input + currentI, 1, 1, filePtr);
-            }
-
-            //fread(input, INPUT_SIZE, 1, filePtr);
-            createDataPacket(counter, currentI, frame, input);
+            fread(input, INPUT_SIZE, 1, filePtr);
+            createDataPacket(counter, INPUT_SIZE, frame, input);
             if (llwrite(frame, INPUT_SIZE + 4) == -1) {
                 printf("Error sending data packet\n");
             }
